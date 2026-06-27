db85eb56adc91f5a426c9015d98801fd0d89d820
Kevin <changho105@gmail.com>
2026-06-27 21:44:10 +0900

    기록장 날짜 오름차순 복원, 오늘로 자동 스크롤

--- js/app.js
+++ js/app.js
@@ -1779,9 +1779,9 @@ function _xlLoad(){
   }
   var c=$id('log-cards'); c.innerHTML=''; _cardSeq=0;
   $id('log-empty').style.display='none';
-  days.slice().reverse().forEach(function(d){ c.appendChild(_makeCard(d)); });
-  // 맨 위(오늘)로 스크롤
-  var pages=$id('pages'); if(pages) setTimeout(function(){ pages.scrollTop=0; },50);
+  days.forEach(function(d){ c.appendChild(_makeCard(d)); });
+  // 맨 아래(오늘)로 자동 스크롤
+  var pages=$id('pages'); if(pages) setTimeout(function(){ pages.scrollTop=pages.scrollHeight; },50);
 }
 
 function exportExcel(){
